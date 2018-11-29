e6067a963cc9c78b6d21b0af6b81b62bda9f607f
Highvirus <yjake86@yahoo.com>
2018-11-29 15:24:25 -0600

    first fit algorithm added

--- Memory.h
+++ Memory.h
@@ -141,9 +141,12 @@ class Memory
         int timeStepCounter = 0;
         int lastCounter = 0;
         int positionCounter = getTracePosition();
+
+        //vars getting data from trace
         int processBeginTime = 0;
         int processMemSize = 0;
         int processEndTime = 0;
+
         int tempMemSize = 0;
         int totalTime = 0;
 
@@ -160,7 +163,7 @@ class Memory
         while (timeStepCounter < timeStep) {
             if (positionCounter >= traceData.size()) {
                 setTimeStepNumber(getLastTimeStep());
-                cout << "Current Time Step5: " << getTimeStep() << endl;
+                cout << "Current Time Step: " << getTimeStep() << endl;
                 cout << "There is not enough trace data to continue." << endl;
                 printTraceMap(traceMapData);
                 return;
@@ -174,7 +177,108 @@ class Memory
                 timeStepCounter++;
                 continue;
             }
-            
+
+            //Set default vars
+            traceMapData = getTraceMap();
+            firstPosition = -1;
+            secondPosition = -1;
+            consecutiveCounter = 0;
+
+            //Get the data from the traceData
+            processBeginTime = traceData[positionCounter];
+            processMemSize = traceData[positionCounter+1];
+            processEndTime = traceData[positionCounter+2];
+            tempMemSize = processMemSize + totalProcessMemSize;
+
+            //Check if the memory size is greater then the allocated memory size
+            if (tempMemSize > memSize) {
+                //Trash the proceess and continue on
+                timeStepCounter++;
+                positionCounter+=3;
+                lastCounter+=3;
+                continue;
+            } else {
+                totalProcessMemSize = tempMemSize;
+                totalTime = processBeginTime + processEndTime;
+            }
+
+            //Find the ideal locations for the trace map
+            for (int i = 0; i < memSize; i++) {
+                if (processMemSize == consecutiveCounter) {
+                    break;
+                }
+                if (traceMapData[i] == 0) {
+                    if (firstPosition == -1) {
+                        firstPosition = i;
+                    }
+                    secondPosition = i;
+                    consecutiveCounter++;
+                } else if (firstPosition != -1 && traceMapData[i] != 0) {
+                    firstPosition = -1;
+                    secondPosition = -1;
+                    consecutiveCounter = 0;
+                }
+            }
+            if (firstPosition != -1 && secondPosition != -1 && consecutiveCounter == processMemSize) {
+                for (int i = firstPosition; i <= secondPosition; i++) {
+                    traceMapData[i] = totalTime;
+                }
+                setTraceMap(traceMapData);
+            }
+
+            timeStepCounter++;
+            positionCounter+=3;
+            lastCounter+=3;
+        }
+        cout << "Current Time Step: " << timeStep << endl;
+        setTracePosition(positionCounter);
+        setLastTimeStep(timeStep);
+        printTraceMap(traceMapData);   
+    }
+
+    void bestFitAlgorithm(vector<int> traceData, vector<int> traceMapData, int memSize, int timeStep) {
+        int totalProcessMemSize = 0;
+        int timeStepCounter = 0;
+        int lastCounter = 0;
+        int positionCounter = getTracePosition();
+
+        //vars getting data from trace
+        int processBeginTime = 0;
+        int processMemSize = 0;
+        int processEndTime = 0;
+
+        int tempMemSize = 0;
+        int totalTime = 0;
+
+        int consecutiveCounter = 0;
+        int firstPosition = -1;
+        int secondPosition = -1;
+
+        cout << "Previous Time Step: " << getLastTimeStep() << endl;
+        if (timeStep == 0) {
+            printTraceMap(traceMapData);
+            return;
+        }
+
+        while (timeStepCounter < timeStep) {
+            if (positionCounter >= traceData.size()) {
+                setTimeStepNumber(getLastTimeStep());
+                cout << "Current Time Step: " << getTimeStep() << endl;
+                cout << "There is not enough trace data to continue." << endl;
+                printTraceMap(traceMapData);
+                return;
+            }
+            //Clean out the old processes at the current timestep
+            int cleaned = cleanTraceMap(traceMapData, timeStepCounter+1);
+            totalProcessMemSize = totalProcessMemSize - cleaned;
+
+            if (positionCounter > lastCounter) {
+                lastCounter+=3;
+                timeStepCounter++;
+                continue;
+            }
+
+            //Set default vars
             traceMapData = getTraceMap();
             firstPosition = -1;
             secondPosition = -1;
@@ -228,9 +332,10 @@ class Memory
         cout << "Current Time Step: " << timeStep << endl;
         setTracePosition(positionCounter);
         setLastTimeStep(timeStep);
-        printTraceMap(traceMapData);
-        
+        printTraceMap(traceMapData);   
     }
+
+
     
 };
 
